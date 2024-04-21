1353fed97e95a5aa2e297f247104ad64864575d0
Sachin Jadhav <sjd3333@umd.edu>
2024-04-21 12:07:47 -0400

    added data loader file

--- MPNet/asas.py
+++
@@ -1,8 +0,0 @@
-path_not_generated = {0:[]}
-image_number = 1
-start = 9
-goal = 10
-if image_number not in path_not_generated:
-    path_not_generated[image_number] = [(start, goal)]
-else:
-    path_not_generated[image_number].append((start, goal))
