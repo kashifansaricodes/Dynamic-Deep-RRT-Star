16b79fc38b1789ed7ec2bbd8ad30aa6206ece326
Sachin Jadhav <sjd3333@umd.edu>
2024-04-27 00:02:50 -0400

    architecture changes

--- MPNet/train.py
+++ MPNet/train.py
@@ -1,101 +1,85 @@
+import torch
+import torch.utils.data as data
+import os
+import pickle
+import numpy as np
+import nltk
+from PIL import Image
+import os.path
+import random
+from torch.autograd import Variable
+import torch.nn as nn
+import math
+import cv2
+from torchvision import datasets, transforms
+import pandas as pd
+import numpy as np
 import argparse
 import torch
 import torch.nn as nn
 import numpy as np
 import os
 import pickle
-from Data_loader import load_dataset 
-from model import MLP 
 from torch.autograd import Variable 
 import math
+import csv
+import MLP
+from data_loader import dataset,targets
 
 def to_var(x, volatile=False):
-	if torch.cuda.is_available():
-		x = x.cuda()
-	return Variable(x, volatile=volatile)
+    if torch.cuda.is_available():
+        x = x.to(torch.device("cuda"))
+    return x
 
 def get_input(i,data,targets,bs):
+    if i+bs<len(data):
+        bi=data[i:i+bs]
+        bt=targets[i:i+bs]
+    else:
+        bi=data[i:]
+        bt=targets[i:]
+    return torch.from_numpy(bi),torch.from_numpy(bt)
 
-	if i+bs<len(data):
-		bi=data[i:i+bs]
-		bt=targets[i:i+bs]	
-	else:
-		bi=data[i:]
-		bt=targets[i:]
-		
-	return torch.from_numpy(bi),torch.from_numpy(bt)
+#dataset,targets= load_dataset()
+#I have to yet write the architecture for the model
+mlp = MLP(32, 2)
 
-
+if torch.cuda.is_available():
+    mlp = mlp.to(torch.device("cuda"))
     
-def main(args):
-	# Create model directory
-	if not os.path.exists(args.model_path):
-		os.makedirs(args.model_path)
-    
-    
-	# Build data loader
-	dataset,targets= load_dataset() 
-	
-	# Build the models
-	mlp = MLP(args.input_size, args.output_size)
-    
-	if torch.cuda.is_available():
-		mlp.cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adagrad(mlp.parameters())
 
-	# Loss and Optimizer
-	criterion = nn.MSELoss()
-	optimizer = torch.optim.Adagrad(mlp.parameters()) 
-    
-	# Train the Models
-	total_loss=[]
-	print(len(dataset))
-	print(len(targets))
-	sm=100 # start saving models after 100 epochs
-	for epoch in range(args.num_epochs):
-		print("epoch" + str(epoch))
-		avg_loss=0
-		for i in range (0,len(dataset),args.batch_size):
-			# Forward, Backward and Optimize
-			mlp.zero_grad()			
-			bi,bt= get_input(i,dataset,targets,args.batch_size)
-			bi=to_var(bi)
-			bt=to_var(bt)
-			bo = mlp(bi)
-			loss = criterion(bo,bt)
-			avg_loss=avg_loss+loss.data[0]
-			loss.backward()
-			optimizer.step()
-		print("--average loss:")
-		print(avg_loss/(len(dataset)/args.batch_size))
-		total_loss.append(avg_loss/(len(dataset)/args.batch_size))
-		# Save the models
-		if epoch==sm:
-			model_path='mlp_100_4000_PReLU_ae_dd'+str(sm)+'.pkl'
-			torch.save(mlp.state_dict(),os.path.join(args.model_path,model_path))
-			sm=sm+50 # save model after every 50 epochs from 100 epoch ownwards
-	torch.save(total_loss,'total_loss.dat')
-	model_path='mlp_100_4000_PReLU_ae_dd_final.pkl'
-	torch.save(mlp.state_dict(),os.path.join(args.model_path,model_path))
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--model_path', type=str, default='./models/',help='path for saving trained models')
-	parser.add_argument('--no_env', type=int, default=50,help='directory for obstacle images')
-	parser.add_argument('--no_motion_paths', type=int,default=2000,help='number of optimal paths in each environment')
-	parser.add_argument('--log_step', type=int , default=10,help='step size for prining log info')
-	parser.add_argument('--save_step', type=int , default=1000,help='step size for saving trained models')
+total_loss=[]
+sm=100 
+#training the model for 1000 epochs
+for epoch in range(1000):
+    #print ("epoch" + str(epoch))
+    avg_loss=0
+    for i in range (0,len(dataset),100):
+        mlp.zero_grad()
+        bi,bt= get_input(i,dataset,targets,100)
+        bi=to_var(bi)
+        bt=to_var(bt)
+        bo = mlp(bi)
+        loss = criterion(bo,bt)
+        #avg_loss=avg_loss+loss.data[0]
+        loss.backward()
+        optimizer.step()
+        
+    print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')  
+#     print "--average loss:"
+#     print avg_loss/(len(dataset)/args.batch_size)
+    total_loss.append(avg_loss/(len(dataset)/100))
+    # Save the models
+    if epoch==sm:
+        model_path='model'+str(sm)+'.pkl'
+        save_path = '/home/sj/Deep-RRT-Star-Implementation/weights/model_weights'
+        torch.save(mlp.state_dict(),os.path.join(save_path , model_path))
+        sm=sm+50 # save model after every 50 epochs from 100 epoch ownwards
 
-	# Model parameters
-	parser.add_argument('--input_size', type=int , default=32, help='dimension of the input vector')
-	parser.add_argument('--output_size', type=int , default=2, help='dimension of the input vector')
-	parser.add_argument('--hidden_size', type=int , default=256, help='dimension of lstm hidden states')
-	parser.add_argument('--num_layers', type=int , default=4, help='number of layers in lstm')
-
-	parser.add_argument('--num_epochs', type=int, default=500)
-	parser.add_argument('--batch_size', type=int, default=100)
-	parser.add_argument('--learning_rate', type=float, default=0.0001)
-	args = parser.parse_args()
-	print(args)
-	main(args)
-
-
-
+            
+#Following are the commands for saving the final model.            
+model_path='/home/sj/Deep-RRT-Star-Implementation/weights/model_weights/final_model.pkl'
+torch.save(mlp.state_dict() , model_path)
+        
